041c615435384b091c5b4d9854c5ab8bf9c2208d
Stephen Lauck <stpehen.lauck@gmail.com>
2012-11-08 15:42:59 -0800

    add smartos/solaris chef-client cron support

--- recipes/cron.rb
+++ recipes/cron.rb
@@ -48,37 +48,81 @@ end
   end
 end
 
-dist_dir, conf_dir = value_for_platform(
-                                        ["ubuntu", "debian"] => { "default" => ["debian", "default"] },
-                                        ["redhat", "centos", "fedora", "scientific", "amazon"] => { "default" => ["redhat", "sysconfig"]}
-                                        )
+case node['platform']
+when "smartos"
+  local_path = ::File.join(Chef::Config[:file_cache_path], "/")
+  
+  directory "/etc/svc/method" do
+    action :create
+  end
 
-# let's create the service file so the :disable action doesn't fail
-template "/etc/init.d/chef-client" do
-  source "#{dist_dir}/init.d/chef-client.erb"
-  mode 0755
-  variables(
-            :client_bin => client_bin
-            )
+  template "/etc/svc/method/chef-client" do
+     source "smartos/chef-client.erb"
+     owner "root"
+     group "root"
+     mode "0777"
+     notifies :restart, "service[chef-client]"
+   end
+
+  template (local_path + "chef-client.xml") do
+    source "smartos/manifest.xml.erb"
+    owner "root"
+    group "root"
+    mode "0644"
+    notifies :run, "execute[load chef-client manifest]", :immediately
+  end
+
+  cron "chef-client" do
+    minute node['chef_client']['cron']['minute']	
+    hour	node['chef_client']['cron']['hour']
+    path node['chef_client']['cron']['path'] if node['chef_client']['cron']['path']
+    user	"root"
+    shell	"/bin/bash"
+    command "/opt/local/bin/sleep `/opt/local/bin/expr $RANDOM \\% 90` &> /dev/null ; #{client_bin} &> /dev/null "
+  end
+  
+  execute "load chef-client manifest" do
+    action :nothing
+    command "svccfg import #{local_path}chef-client.xml"
+    notifies :restart, "service[chef-client]"
+  end
+  
+  service "chef-client" do
+    action [:enable, :start]
+    provider Chef::Provider::Service::Solaris
+  end
+else
+  dist_dir, conf_dir = value_for_platform(
+                                          ["ubuntu", "debian"] => { "default" => ["debian", "default"] },
+                                          ["redhat", "centos", "fedora", "scientific", "amazon"] => { "default" => ["redhat", "sysconfig"]}
+                                          )
+
+  # let's create the service file so the :disable action doesn't fail
+  template "/etc/init.d/chef-client" do
+    source "#{dist_dir}/init.d/chef-client.erb"
+    mode 0755
+    variables(
+              :client_bin => client_bin
+              )
+  end
+
+  template "/etc/#{conf_dir}/chef-client" do
+    source "#{dist_dir}/#{conf_dir}/chef-client.erb"
+    mode 0644
+  end
+
+  service "chef-client" do
+    supports :status => true, :restart => true
+    action [:disable, :stop]
+  end
+
+  cron "chef-client" do
+    minute node['chef_client']['cron']['minute']	
+    hour	node['chef_client']['cron']['hour']
+    path node['chef_client']['cron']['path'] if node['chef_client']['cron']['path']
+    user	"root"
+    shell	"/bin/bash"
+    command "/bin/sleep `/usr/bin/expr $RANDOM \\% 90` &> /dev/null ; #{client_bin} &> /dev/null "
+  end
 end
-
-template "/etc/#{conf_dir}/chef-client" do
-  source "#{dist_dir}/#{conf_dir}/chef-client.erb"
-  mode 0644
-end
-
-service "chef-client" do
-  supports :status => true, :restart => true
-  action [:disable, :stop]
-end
-
-cron "chef-client" do
-  minute node['chef_client']['cron']['minute']	
-  hour	node['chef_client']['cron']['hour']
-  path node['chef_client']['cron']['path'] if node['chef_client']['cron']['path']
-  user	"root"
-  shell	"/bin/bash"
-  command "/bin/sleep `/usr/bin/expr $RANDOM \\% 90` &> /dev/null ; #{client_bin} &> /dev/null "
-end
-
 
